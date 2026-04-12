73ae8af65e32f1dfb56bb2b55cd80aec92f6153b
Sagar Somjiyani <sagarsomjiyani@gmail.com>
2026-04-12 22:27:12 +0530

    fix: popover caching scroll position bug for internal links

--- quartz/components/scripts/popover.inline.ts
+++ quartz/components/scripts/popover.inline.ts
@@ -30,11 +30,14 @@ async function mouseEnterHandler(
     setPosition(popoverElement as HTMLElement)
 
     if (hash !== "") {
-      const targetAnchor = `#popover-internal-${hash.slice(1)}`
-      const heading = popoverInner.querySelector(targetAnchor) as HTMLElement | null
-      if (heading) {
-        // leave ~12px of buffer when scrolling to a heading
-        popoverInner.scroll({ top: heading.offsetTop - 12, behavior: "instant" })
+      const pInner = popoverElement.querySelector(".popover-inner") as HTMLElement | null
+      if (pInner) {
+        const targetAnchor = `#popover-internal-${hash.slice(1)}`
+        const heading = pInner.querySelector(targetAnchor) as HTMLElement | null
+        if (heading) {
+          // leave ~12px of buffer when scrolling to a heading
+          pInner.scroll({ top: heading.offsetTop - 12, behavior: "instant" })
+        }
       }
     }
   }
