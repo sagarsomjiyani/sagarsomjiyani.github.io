6a6dfe12ff1b40ec1c85c204f4665ef8ce5acf74
Sagar Somjiyani <sagarsomjiyani@gmail.com>
2026-04-12 22:11:35 +0530

    chore: personalize site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Income Tax Act - TDS",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
