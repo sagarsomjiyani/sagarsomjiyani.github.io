2ef71c38fc1effdccfc43b233868d733d5214c93
Sagar Somjiyani <sagarsomjiyani@gmail.com>
2026-04-27 21:41:35 +0530

    Quartz sync: Apr 27, 2026, 9:41 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,8 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          For more information access
+          <a href="https://capkpatel.in/">TDS Tool</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
